74ca01a0e8b0c48ecd6bfd20370a335dd5b0a190
Meghavi <meghavitomar1609@gmail.com>
2022-01-04 15:57:50 +0530

    functions

--- decimalToBinary.java
+++ decimalToBinary.java
@@ -16,10 +16,24 @@ public class decimalToBinary {
         }
         return ans;
     }
+    public static int BinToDec(int n) {
+        int ans = 0;
+        int power = 1;
+        while(n!=0){
+            int rem = n%10;
+            n/=10;
+            ans += rem*power;
+            power = power*2;
+
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        System.out.println(decToBin(n));
+        int Binary = decToBin(n);
+        int Decimal = BinToDec(Binary);
+        System.out.println(Decimal);
         scn.close(); 
     }
 }
